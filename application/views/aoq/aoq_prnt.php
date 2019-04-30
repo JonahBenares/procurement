40951e982b2d18d98e4cc6b201d878f30e638881
projectpixel16 <jasonflor.cenpri@gmail.com>
2019-04-30 10:52:15 +0800

    rfq

---
+++ application/views/aoq/aoq_prnt.php
@@ -0,0 +1,245 @@
+  	<style type="text/css">
+        html, body{
+            background: #2d2c2c!important;
+            font-size:12px!important;
+        }
+        .pad{
+        	padding:0px 250px 0px 250px
+        }
+        @media print{
+        	.pad{
+        	padding:0px 0px 0px 0px
+        	}
+        }
+        .table-bordered>tbody>tr>td, 
+        .table-bordered>tbody>tr>th, 
+        .table-bordered>tfoot>tr>td, 
+        .table-bordered>tfoot>tr>th, 
+        .table-bordered>thead>tr>td, 
+        .table-bordered>thead>tr>th
+        {
+		    border: 1px solid #000!important;
+		}
+		.f13{
+			font-size:13px!important;
+		}
+		.bor-btm{
+			border-bottom: 1px solid #000;
+		}
+		.sel-des{
+			border: 0px!important;
+		}
+		@media print{
+			#prnt_btn{
+				display: none;
+			}
+		}
+		.text-white{
+			color: #fff;
+		}
+    </style>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/animate.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/mixins.css">
+    <div  class="pad">
+    	<form>  
+    		<center>
+		    	<div class="btn-group" id="prnt_btn">
+					<a href="" class="btn btn-success btn-md p-l-100 p-r-100"><span class="fa fa-arrow-left"></span> Back</a>
+					<a  onclick="printPage()" class="btn btn-warning btn-md p-l-100 p-r-100"><span class="fa fa-print"></span> Print</a>
+					<a href="" class="btn btn-primary btn-md p-l-100 p-r-100"><span class="fa fa-floppy-o"></span> Save</a>    				
+				</div>
+				<h4 class="text-white"><b>OUTGOING</b> RFQ</h4>
+			</center>
+	    	<div style="background: #fff;">    		  			
+		    	<table class="table-boSrdered" width="100%" style="border:2px solid #000">
+		    		<tr>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="5" align="center"><img width="150px" src="<?php echo base_url(); ?>assets/img/logo_cenpri.png"></td>
+		    			<td colspan="15"><h4 style="margin: 0px"><b>CENTRAL NEGROS POWER RELIABILITY, INC.</b></h4></td>
+		    		</tr>
+		    		<tr><td class="f13" colspan="20" align="center">Office: 88 Corner Rizal-Mabini Sts., Bacolod City</td></tr>
+		    		<tr><td class="f13" colspan="20" align="center">Tel. No.: (034) 435-1932/476-7382</td></tr>
+		    		<tr><td class="f13" colspan="20" align="center">Telefax: (034) 435-1932</td></tr>
+		    		<tr><td class="f13" colspan="20" align="center">Plant Site: Purok San Jose, Barangay Calumangan, Bago City</td></tr>
+		    		<tr><td class="f13" colspan="20" align="center"><br></td></tr>
+		    		<tr><td colspan="20" align="center"><b>REQUEST FOR QUOTATION</b></td></tr>
+		    		<tr><td class="f13" colspan="20" align="center"><br></td></tr>
+		    		<tr>
+		    			<td class="f13" colspan="2">Date:</td>
+		    			<td class="f13 bor-btm" colspan="8">asdasdas</td>
+		    			<td class="f13" colspan="1"></td>
+		    			<td class="f13" colspan="3">RFQ No. Series:</td>
+		    			<td class="f13 bor-btm" colspan="6">1231277</td>
+		    		</tr>
+		    		<tr><td class="f13" colspan="20" align="center"></td></tr>
+		    		<tr>
+		    			<td class="f13" colspan="2">Supplier:</td>
+		    			<td class="f13 bor-btm" colspan="8">asdasdas</td>
+		    			<td class="f13" colspan="1"></td>
+		    			<td class="f13" colspan="3">Tel. No.:</td>
+		    			<td class="f13 bor-btm" colspan="6">1231277</td>
+		    		</tr>
+		    		<tr><td class="f13" colspan="20" align="center"></td></tr>	    		
+		    		<tr><td class="f13" colspan="20" align="center"><br></td></tr>	    		
+		    		<tr>
+		    			<td colspan="20">
+		    				<table class="table-bordered" width="100%">
+		    					<tr>
+		    						<td class="f13" align="center" width="5%"><b>Qty</b></td>
+		    						<td class="f13" align="center"><b>Unit</b></td>
+		    						<td class="f13" align="center"><b>Item Description</b></td>
+		    						<td class="f13" align="center"><b>Brand/Offer</b></td>
+		    						<td class="f13" align="center"><b>Unit Price</b></td>
+		    					</tr>
+		    					<tr>
+		    						<td class="f13" align="center">23</td>
+		    						<td class="f13" align="center">Unit</td>
+		    						<td class="f13" align="center">Item Description</td>
+		    						<td class="f13" align="center">Brand/Offer</td>
+		    						<td class="f13" align="center">Unit Price</td>
+		    					</tr>
+		    				</table>
+		    			</td>
+		    		</tr>
+		    		<tr><td class="f13" colspan="20" align="center"><br></td></tr>	    		
+
+		    		<tr><td class="f13" colspan="20">1. Quotation must be submitted on or before <input type="date" name="" style="border: 0px solid #000"></td></tr>	    	
+		    		<tr><td class="f13" colspan="20">2. Please Fill - Up :</td></tr>	    	
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="5">- a. Price Validity</td>
+		    			<td class="f13" colspan="3"></td>
+		    			<td class="f13 bor-btm" colspan="7"></td>
+		    			<td class="f13" colspan="3"></td>
+
+		    		</tr>
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="5">- b. Payment Terms</td>
+		    			<td class="f13" colspan="3"></td>
+		    			<td class="f13 bor-btm" colspan="7"></td>
+		    			<td class="f13" colspan="3"></td>
+
+		    		</tr>	
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="5">- c. Date of Delivery</td>
+		    			<td class="f13" colspan="3"></td>
+		    			<td class="f13 bor-btm" colspan="7"></td>
+		    			<td class="f13" colspan="3"></td>
+
+		    		</tr>	
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="5">&nbsp; d. Item's Warranty</td>
+		    			<td class="f13" colspan="3"></td>
+		    			<td class="f13 bor-btm" colspan="7"></td>
+		    			<td class="f13" colspan="3"></td>
+
+		    		</tr>	
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="5">&nbsp; e. Company's TIN Number</td>
+		    			<td class="f13" colspan="3"></td>
+		    			<td class="f13 bor-btm" colspan="7"></td>
+		    			<td class="f13" colspan="3"></td>
+
+		    		</tr>	
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="5">&nbsp; d. Item's Warranty</td>
+		    			<td class="f13" colspan="3"></td>
+		    			<td class="f13 bor-btm" colspan="7"></td>
+		    			<td class="f13" colspan="3"></td>
+
+		    		</tr>	
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="18">&nbsp; f. Vat <input type="checkbox" name=""> ||  non-Vat <input type="checkbox" name=""></td>
+		    		</tr>
+		    		<tr><td class="f13" colspan="20" align="center"><br></td></tr>	
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="4">Prepared by:</td>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="4">Noted by:</td>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="4">Approved by:</td>
+		    			<td class="f13" colspan="2"></td>
+		    		</tr>	  
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13 bor-btm" colspan="4"><br></td>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13 bor-btm" colspan="4"><br></td>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13 bor-btm" colspan="4"><br></td>
+		    			<td class="f13" colspan="2"></td>
+		    		</tr>  	
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="4">
+		    				<input type="" name="" class="sel-des">
+		    			</td>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="4">
+		    				<input type="" name="" class="sel-des">
+		    			</td>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="4">
+		    				<input type="" name="" class="sel-des">
+		    			</td>
+		    			<td class="f13" colspan="2"></td>
+		    		</tr>  	
+		    		<tr><td class="f13" colspan="20" align="center"><br></td></tr>	
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="2">
+		    				Conforme:
+		    			</td>
+		    			<td class="f13 bor-btm" colspan="6"></td>
+		    			<td class="f13" colspan="6"></td>
+		    			<td class="f13" colspan="4">
+		    			</td>
+		    		</tr>  
+		    		<tr>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="2"></td>
+		    			<td class="f13" colspan="7" align="center">Supplier's Signature Over Printed Name</td>
+		    			<td class="f13" colspan="5"></td>
+		    			<td class="f13" colspan="4">
+		    			</td>
+		    		</tr> 
+		    		<tr><td class="f13" colspan="20" align="center"><br></td></tr>		
+		    	</table>		    
+	    	</div>
+    	</form>
+    </div>
+    <script type="text/javascript">
+    	function printPage() {
+		  window.print();
+		}
+    </script>
