dd52553d9148e95c7ecca3f12f58a0c2fadcc3e8
stephinesev <stephineseverino.cenpri@gmail.com>
2019-05-31 11:56:06 +0800

    modified viewhistory

--- application/views/po/view_history.php
+++ application/views/po/view_history.php
@@ -12,7 +12,7 @@
                 </div>
                 <div class="modal-body">
                     <?php foreach($revise AS $r){ ;?>
-                        <a onClick="javascript:window.close(); window.opener.location.href='<?php echo base_url(); ?>po/purchase_order_saved_r/<?php echo $r['po_id'];?>/<?php echo $r['revision_no'];?>';" class="btn btn-link btn-link-shad btn-block"><?php echo $r['po_no'];?>
+                        <a onClick="javascript:window.opener.location.href='<?php echo base_url(); ?>po/purchase_order_saved_r/<?php echo $r['po_id'];?>/<?php echo $r['revision_no'];?>';" class="btn btn-link btn-link-shad btn-block"><?php echo $r['po_no'];?>
                             <span class="pull-right"><?php echo date("Y-m-d", strtotime($r['revised_date']));?></span>
                         </a>
                     <?php } ?>
