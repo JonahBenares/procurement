a419b4705fcdb9e425b137f6df7a9bb95325fe7e
projectpixel16 <jasonflor.cenpri@gmail.com>
2019-06-01 16:26:32 +0800

    aoq five and Dr

--- application/views/aoq/aoq_prnt_five.php
+++ application/views/aoq/aoq_prnt_five.php
@@ -59,6 +59,9 @@
 			background-color: #fff;
 		}
 		@media print{
+			.f10{
+				font-size:14px!important;
+			}
 			html, body{
 	            background: #fff!important;
 	            font-size:12px!important;
@@ -66,14 +69,15 @@
 			#prnt_btn{
 				display: none;
 			}
+			tr td{
+				background-color: #fff;
+				color: #000;
+			}
 			#add_btn{
 				display: none;
 			}
 			.f12{
 				font-size:12px!important;
-			}
-			.f10{
-				font-size:11px!important;
 			}
 			.text-red{
 				color: red!important;
@@ -158,13 +162,23 @@
 			color: #000!important;
 			background: #fff!important;
 		}
+		@media print {
+		   #someName {
+		      width:100%;
+		      /*height:100%;*/
+		      overflow-x:visible;
+		      overflow-y:visible;
+
+      /* Some other stylings */
+   }
+}
     </style>
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/animate.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/mixins.css">
     <script src="<?php echo base_url(); ?>assets/js/all-scripts.js"></script> 
-    <div  class="pad">
+    <div  class="pad " id="printableArea">
 	    	<?php if($saved==1){
 				$url =  base_url().'aoq/aoq_complete';
 			} else {
@@ -175,6 +189,9 @@
     		<div id="prnt_btn">
 	    		<center>
 			    	<div class="btn-group">
+			    		<a id="printReport" class="btn btn-info pull-right " onclick="printDiv('printableArea')">
+								<span  class="fa fa-print"></span>
+						</a>
 						<a href="<?php echo base_url(); ?>aoq/aoq_list" class="btn btn-success btn-md p-l-100 p-r-100"><span class="fa fa-arrow-left"></span> Back</a>
 						<?php if($saved==1){ 
 					
@@ -191,17 +208,14 @@
 					<p class="text-white p-l-250 p-r-250">Instructions: When printing ABSTRACT OF QUOTATION make sure the following options are set correctly -- <u>Browser</u>: Chrome, <u>Layout</u>: Landscape, <u>Paper Size</u>: A4 <u>Margin</u> : Custom (top: 0.11" , right:1.25", bottom: 0.11", left: 0.11") <u>Scale</u>: 100 and the option: Background graphics is checked</p>
 				</center>
 			</div>
-	    	<div style="background: #fff;" <?php echo (($served==1) ? 'class="served"' : ''); ?>>    		  			
-		    	<table class="table-bodrdered" width="300%" style="background: #fff;border: 1px solid #000">
+	    	<div id="someName" style="width:2000px;background: #fff;" <?php echo (($served==1) ? 'class="served"' : ''); ?>>    		  			
+		    	<table class="table-bordesred" width="100%" style="background: #fff;border: 1px solid #000">
 		    		<tr>
+		    			<td width="1%"><br></td>
 		    			<td width="3%"><br></td>
 		    			<td width="3%"><br></td>
-		    			<td width="3%"><br></td>
-		    			<td width="3%"><br></td>
-		    			<td width="3%"><br></td>
-		    			<td width="3%"><br></td>
-		    			<td width="3%"><br></td>
-		    			<td width="3%"><br></td>
+		    			<td width="1%"><br></td>
+		    			<td width="1%"><br></td>
 		    			<td width="3%"><br></td>
 		    			<td width="3%"><br></td>
 		    			<td width="3%"><br></td>
@@ -228,34 +242,34 @@
 		    			<td width="3%"><br></td>
 		    			<td width="3%"><br></td>
 		    		</tr>		    	
-		    		<tr><td colspan="33" class="f10"  align="center"><h5><b>ABSTRACT OF QUOTATION</b></h5></td></tr>
+		    		<tr><td colspan="30" class="f10"  align="center"><h5><b>ABSTRACT OF QUOTATION</b></h5></td></tr>
 		    		<?php foreach($head AS $h) { ?>
 		    		<tr>
 		    			<td colspan="4" class="f12" align="right">Department: &nbsp;</td>
-		    			<td colspan="13" class="f12" ><?php echo $h['department']; ?></td>		    			
+		    			<td colspan="12" class="f12" ><?php echo $h['department']; ?></td>		    			
 		    			<td colspan="3" class="f12" align="right">Date: &nbsp;</td>
-		    			<td colspan="13" class="f12" ><?php echo date('F j, Y', strtotime($h['aoq_date'])); ?></td>
+		    			<td colspan="11" class="f12" ><?php echo date('F j, Y', strtotime($h['aoq_date'])); ?></td>
 		    		</tr>	
 		    		<tr>
 		    			<td colspan="4" class="f12" align="right">Purpose: &nbsp;</td>
-		    			<td colspan="13" class="f12" ><?php echo $h['purpose']; ?></td>		    			
+		    			<td colspan="12" class="f12" ><?php echo $h['purpose']; ?></td>		    			
 		    			<td colspan="3"class="f12" align="right">PR #: &nbsp;</td>
-		    			<td colspan="13" class="f12" ><?php echo $h['pr']; ?></td>
+		    			<td colspan="11" class="f12" ><?php echo $h['pr']; ?></td>
 		    		</tr>
 		    		<tr>
 		    			<td colspan="4" class="f12" align="right">Enduse: &nbsp;</td>
-		    			<td colspan="13" class="f12" ><?php echo $h['enduse']; ?></td>		    			
+		    			<td colspan="12" class="f12" ><?php echo $h['enduse']; ?></td>		    			
 		    			<td colspan="3"class="f12" align="right">Date Needed: &nbsp;</td>
-		    			<td colspan="13" class="f12" ><?php echo date('F j, Y', strtotime($h['date_needed'])); ?></td>
+		    			<td colspan="11" class="f12" ><?php echo date('F j, Y', strtotime($h['date_needed'])); ?></td>
 		    		</tr>	
 		    		<tr>
 		    			<td colspan="4" class="f12"  align="right">Requested by: &nbsp;</td>
-		    			<td colspan="29" class="f12" ><?php echo $h['requested']; ?></td>
+		    			<td colspan="26" class="f12" ><?php echo $h['requested']; ?></td>
 		    		</tr>
 		    		<?php } ?>
-		    		<tr><td class="f10" colspan="33" align="center"><br></td></tr>
+		    		<tr><td class="f10" colspan="30" align="center"><br></td></tr>
 		    		<tr>
-		    			<td colspan="8" class="f10"  align="center">		    				
+		    			<td colspan="5" class="f10"  align="center">		    				
 							<?php if($saved==0){ ?>
 		    				<button id="add_btn" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-id="<?php echo $aoq_id; ?>">
 							<span class="fa fa-plus"></span> Add Item
@@ -273,7 +287,7 @@
 		    		</tr>
 		    		<tr>
 		    			<td class="f9 table-borbold "align="center"><b class="p-r-10 p-l-10">#</td>
-		    			<td colspan="5" class="f9 table-borbold" align="center"><b>DESCRIPTION</td>
+		    			<td colspan="2" class="f9 table-borbold" align="center"><b>DESCRIPTION</td>
 		    			<td class="f9 table-borbold" align="center"><b>QTY</td>
 		    			<td class="f9 table-borbold" align="center"><b>UOM</td>
 
@@ -318,7 +332,7 @@
 						 ?>
 		    		<tr>
 		    			<td class="f10 table-borreg" align="center"><?php echo $x; ?></td>
-		    			<td colspan="5" class="f10 table-borreg" align="left"> <?php echo $it['item']; ?></td>
+		    			<td colspan="2" class="f10 table-borreg" align="left"> <?php echo $it['item']; ?></td>
 		    			<td class="f10 table-borreg" align="center"><?php echo $it['qty']; ?></td>
 		    			<td class="f10 table-borreg" align="center"><?php echo $it['uom']; ?>asd</td>
 		    		<?php foreach($supplier AS $sup){ ?>
@@ -328,9 +342,9 @@
 		    				$c=1;
 			    				foreach($CI->get_all_rfq_items($sup['supplier_id'], $it['item_id'],$sup['rfq_id']) AS $allrfq) { 
 			    					$amount = $it['qty'] *$allrfq->unit_price; ?>
-			    				   	<table class="table-bordered" width="100%" style='border:0px solid #000;'>	
+			    				   	<table class="table-borsdered" width="100%" style='border:0px solid #000;'>	
 			    				   		<tr>
-							    			<td width="9%"><br></td>
+							    			<td width="9%"></td>
 							    			<td width="9%"></td>
 							    			<td width="9%"></td>
 							    			<td width="9%"></td>
@@ -342,7 +356,7 @@
 							    			<td width="9%"></td>							    			
 							    		</tr>						
 				    					<tr>
-					    					<td colspan="3" class="bor-btm bor-right  f10">
+					    					<td colspan="4" class="bor-btm bor-right  f10">
 					    						<b class="text-red nomarg">
 					    				 			<?php echo $allrfq->offer; ?>,
 					    						</b> <?php echo $CI->get_name("item_name", "item", "item_id", $allrfq->item_id); ?></td>
@@ -354,7 +368,7 @@
 				    							<?php } ?>
 					    					</td>
 					    					<td colspan="2" class="bor-btm bor-right <?php echo (($reco_supplier == $sup['supplier_id'] && $reco_offer == $allrfq->offer) ? ' green-back' : ''); ?> " align="center"><?php echo number_format($amount,2); ?></td>
-					    					<td colspan="3" class="bor-btm bor-right text-red ">
+					    					<td colspan="2" class="bor-btm bor-right text-red ">
 					    						<?php if($saved=='1' && $completed==0){ ?>
 					    						<textarea cols="4" rows="3" name='comments<?php echo $b; ?>' style=' border: 0px'></textarea>
 					    						<input type='hidden' name='offer<?php echo $b; ?>' value="<?php echo $allrfq->offer; ?>">
@@ -394,7 +408,7 @@
 		    			<input type='hidden' name='count_item' value='<?php echo $a; ?>'>
 		    		<tr>
 		    			<td class="f10 table-borreg" align="center"></td>
-		    			<td colspan="5" class="f10 table-borreg" align="left"></td>
+		    			<td colspan="2" class="f10 table-borreg" align="left"></td>
 		    			<td class="f10 table-borreg" align="center"></td>
 		    			<td class="f10 table-borreg" align="center"></td>
 
@@ -427,7 +441,7 @@
 		    		</tr>
 		    		<tr>
 		    			<td class="f10 table-borreg" align="center"></td>
-		    			<td colspan="7" class="f10 table-borreg text-red" align="center"><b>REMARK</b></td>
+		    			<td colspan="4" class="f10 table-borreg text-red" align="center"><b>REMARK</b></td>
 
 		    			<!-- loop ka here -->
 		    			<td colspan="5" class="f10 table-borreg" align="left"><br></td>
@@ -438,10 +452,10 @@
 		    			<td colspan="5" class="f10 table-borreg" align="left"><br></td>
 		    			<td colspan="5" class="f10 table-borreg" align="left"><br></td>
 		    		</tr>
-		    		<tr><td class="f10" colspan="33" align="center"><br></td></tr>
+		    		<tr><td class="f10" colspan="30" align="center"><br></td></tr>
 		    		<tr>
 		    			<td class="" align="center">a.</td>
-		    			<td colspan="7" class="f10" align="center">Price Validity</td>
+		    			<td colspan="4" class="f10" align="center">Price Validity</td>
 		    			<?php foreach($supplier AS $sup){ ?>
 		    			<td colspan="4" class="f10 bor-btm" align="left"><?php echo $sup['validity']; ?><br></td>
 		    			<td colspan="1" class="f10" align="left"><br></td>
@@ -450,7 +464,7 @@
 		    		</tr>
 		    		<tr>
 		    			<td class="" align="center">b.</td>
-		    			<td colspan="7" class="f10" align="center">Payment Terms</td>
+		    			<td colspan="4" class="f10" align="center">Payment Terms</td>
 		    			<?php foreach($supplier AS $sup){ ?>
 		    			<td colspan="4" class="f10 bor-btm" align="left"><?php echo $sup['terms']; ?><br></td>
 		    			<td colspan="1" class="f10" align="left"><br></td>
@@ -459,7 +473,7 @@
 		    		</tr>
 		    		<tr>
 		    			<td class="" align="center">c.</td>
-		    			<td colspan="7" class="f10" align="center">Date of Delivery</td>
+		    			<td colspan="4" class="f10" align="center">Date of Delivery</td>
 		    			<?php foreach($supplier AS $sup){ ?>
 		    			<td colspan="4" class="f10 bor-btm" align="left"><?php echo (($sup['delivery']!='') ? date('F j, Y', strtotime($sup['delivery'])) : ''); ?><br></td>
 		    			<td colspan="1" class="f10" align="left"><br></td>
@@ -468,33 +482,33 @@
 		    		</tr>
 		    		<tr>
 		    			<td class="" align="center">d.</td>
-		    			<td colspan="7" class="f10" align="center">Item's Warranty</td>
+		    			<td colspan="4" class="f10" align="center">Item's Warranty</td>
 		    			<?php foreach($supplier AS $sup){ ?>
 		    			<td colspan="4" class="f10 bor-btm" align="left"><?php echo $sup['warranty']; ?><br></td>
 		    			<td colspan="1" class="f10" align="left"><br></td>
 		    			<?php } ?>
 		    			
 		    		</tr>
-		    		<tr><td class="f10" colspan="33" align="center"><br></td></tr>
+		    		<tr><td class="f10" colspan="30" align="center"><br></td></tr>
 		    		<tr>
 		    			<td colspan="2"  class="" align="center"></td>
 		    			<td colspan="5" class="f10" align="center">Prepared by:</td>
 		    			<td colspan="2" class="f10" align="left"><br></td>
-		    			<td colspan="6" class="f10" align="center">Award Recommended by:</td>
+		    			<td colspan="5" class="f10" align="center">Award Recommended by:</td>
 		    			<td colspan="2" class="f10" align="left"><br></td>
-		    			<td colspan="6" class="f10" align="center">Noted by:</td>
+		    			<td colspan="5" class="f10" align="center">Noted by:</td>
 		    			<td colspan="2" class="f10" align="left"><br></td>
-		    			<td colspan="6" class="f10" align="center">Approved by:</td>
+		    			<td colspan="5" class="f10" align="center">Approved by:</td>
 		    			<td colspan="2" class="f10" align="left"><br></td>
 		    		</tr>
-		    		<tr><td class="f10" colspan="33" align="center"><br></td></tr>
+		    		<tr><td class="f10" colspan="30" align="center"><br></td></tr>
 		    		<tr>
 		    			<td colspan="2"  class="" align="center"></td>
 		    			<td colspan="5" class="f10 bor-btm" align="center"><?php echo $_SESSION['fullname']; ?></td>
 		    			<td colspan="2" class="f10" align="left"><br></td>
-		    			<td colspan="6" class="f10 bor-btm" align="center"><?php echo $requested; ?></td>
+		    			<td colspan="5" class="f10 bor-btm" align="center"><?php echo $requested; ?></td>
 		    			<td colspan="2" class="f10" align="left"><br></td>
-		    			<td colspan="6" class="f10 bor-btm" align="center">
+		    			<td colspan="5" class="f10 bor-btm" align="center">
 		    				<?php if($saved==0){ ?>
 		    			<select name='approved' required>
 			    			<option value=''>-Select-</option>
@@ -507,7 +521,7 @@
 		    			 } ?>
 		    			</td>
 		    			<td colspan="2" class="f10" align="left"><br></td>
-		    			<td colspan="6" class="f10 bor-btm" align="center">
+		    			<td colspan="5" class="f10 bor-btm" align="center">
 		    				<?php if($saved==0){ ?>
 		    				<select name='noted' required>
 			    			<option value=''>-Select-</option>
@@ -521,7 +535,7 @@
 		    			</td>
 		    			<td colspan="2" class="f10" align="left"><br></td>
 		    		</tr>
-		    		<tr><td class="f10" colspan="33" align="center"><br></td></tr>
+		    		<tr><td class="f10" colspan="30" align="center"><br></td></tr>
 		    		<tr>
 		    			<td colspan="2"  class="" align="center"></td>
 		    			<td colspan="2"  class="" align="center"></td>
@@ -530,7 +544,7 @@
 		    			<td colspan="1"  class="green-back p-l-5 p-r-5" align="center"></td>
 		    			<td colspan="1"  class="" align="center"></td>
 		    			<td colspan="4"  class="" align="left">Recommended Award</td>
-		    			<td colspan="19"  class="" align="center"></td>
+		    			<td colspan="16"  class="" align="center"></td>
 
 		    		</tr>
 		    		<tr>
@@ -541,10 +555,10 @@
 		    			<td colspan="1"  class="yellow-back p-l-5 p-r-5" align="center"></td>
 		    			<td colspan="1"  class="" align="center"></td>
 		    			<td colspan="4"  class="" align="left">Lowest Price</td>
-		    			<td colspan="19"  class="" align="center"></td>
+		    			<td colspan="16"  class="" align="center"></td>
 
 		    		</tr>
-		    		<tr><td class="f10" colspan="33" align="center"><br></td></tr>
+		    		<tr><td class="f10" colspan="30" align="center"><br></td></tr>
 
 		    	</table>
 		    	    
@@ -595,4 +609,12 @@
     	function printPage() {
 		  window.print();
 		}
+
+		function printDiv(divName) {
+	     var printContents = document.getElementById(divName).innerHTML;
+	     var originalContents = document.body.innerHTML;
+	     document.body.innerHTML = printContents;
+	     window.print();
+	     document.body.innerHTML = originalContents;
+	}
     </script>
