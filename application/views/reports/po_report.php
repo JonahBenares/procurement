ccdca8b02cc87c644e634116957f34115140cbe5
projectpixel16 <jasonflor.cenpri@gmail.com>
2019-06-20 10:52:53 +0800

    POandpr report

--- application/views/reports/po_report.php
+++ application/views/reports/po_report.php
@@ -62,6 +62,9 @@
                                 </h1>
                                 <small class="p-l-25">&nbsp;PURCHASE ORDER</small> 
                                 <div class="sparkline8-outline-icon">
+                                    <a type='button' class="btn btn-custon-three btn-info"  data-toggle="modal" data-target="#filter_pr"> 
+                                        <span class="fa fa-print p-l-0"></span> Export to Excel
+                                    </a>
                                     <a type='button' class="btn btn-custon-three btn-success"  data-toggle="modal" data-target="#filter_pr"> 
                                         <span class="fa fa-filter p-l-0"></span> Filter
                                     </a>
